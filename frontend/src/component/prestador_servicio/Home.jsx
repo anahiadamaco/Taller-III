63ea24c2d472f8fbb97a7ade5820478253ae8a03
IssPame19 <issabelpamela4e@gmail.com>
2024-09-01 18:15:48 -0400

    Pequeño avance

--- frontend/src/component/prestador_servicio/Home.jsx
+++ frontend/src/component/prestador_servicio/Home.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
 
 function Home() {
-  return (
-    <div>
-      {/* Header provisorio mientras se termina el navbar */}
-      <header>
-        <div> Logo Temuco </div>
-        <div> Bienvenid@ (Nombre) </div>
-        <div> Cerrar sesion </div>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            {/* Header provisorio */}
+            <header>
+                <div> Logo Temuco </div>
+                <div> Bienvenid@ (Nombre) </div>
+                <div> Cerrar sesion </div>
+            </header>
+
+            <section>
+                <div>
+                    <h2> Resumen de citas semanales </h2>
+                </div>
+
+                <div>
+                    <h2> Horarios disponibles </h2>
+                </div>
+
+                <div>
+                    <button> Editar horario </button>
+                    <button> Editar citas </button>
+                    <button> Editar perfil </button>
+                </div>
+            </section>
+
+            {/* Footer provisorio */}
+            <footer>
+                <div> Mensaje </div>
+                <div> Contacto muni </div>
+            </footer>
+        </div>
+    );
 }
 
 export default Home;
--- frontend/src/component/prestador_servicio/Psicologia.jsx
+++ frontend/src/component/prestador_servicio/Psicologia.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 
 function Psicologia() {
-  return (
-    <div>
-    {/* Header provisorio mientras se termina el navbar */}
-        <header>
-            <div> Logo Muni Temuco </div>
-            
-            <div> Asistente social </div>
-            <div> Asistente juridico </div>
-            <div> Fonoaudiologia </div>
-            <div> Podologia </div>
-            <div> Peluqueria </div>
-            <div> Kinesiologia </div>
-            <div> Psicologia </div>
+    return (
+        <div>
+        {/* Header provisorio mientras se termina el navbar */}
+            <header>
+                <div> Logo Muni Temuco </div>
+                
+                <div> Asistente social </div>
+                <div> Asistente juridico </div>
+                <div> Fonoaudiologia </div>
+                <div> Podologia </div>
+                <div> Peluqueria </div>
+                <div> Kinesiologia </div>
+                <div> Psicologia </div>
 
-            <div> Cerrar sesion </div>
-        </header>
-    </div>
-  );
+                <div> Cerrar sesion </div>
+            </header>
+        </div>
+    );
 }
 
 export default Psicologia;
