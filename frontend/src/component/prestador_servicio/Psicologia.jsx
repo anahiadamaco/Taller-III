197f61b39c78c989a773407fdcb75035fbab90a1
IssPame19 <issabelpamela4e@gmail.com>
2024-09-01 23:04:49 -0400

    Pagina Psicología

--- frontend/src/component/prestador_servicio/Psicologia.jsx
+++ frontend/src/component/prestador_servicio/Psicologia.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Psicologia() {
     return (
         <div>
-        {/* Header provisorio mientras se termina el navbar */}
+            {/* Header provisorio mientras se termina el navbar */}
             <header>
                 <div> Logo Muni Temuco </div>
                 
@@ -16,7 +16,36 @@ function Psicologia() {
                 <div> Psicologia </div>
 
                 <div> Cerrar sesion </div>
+
+                <div>
+                    <button> Editar horario </button>
+                    <button> Editar citas </button>
+                    <button> Editar perfil </button>
+                </div>
             </header>
+
+            <section>
+                <div>
+                    <h3> Grafico de atencion mensual </h3>
+                    <h5> x: dias del mes, y: num de personas por dias </h5>
+                </div>
+
+                <div>
+                    <h3> Perfil prestador servicio </h3>
+                    <div>
+                        <h4> Nombre: </h4>
+                        <h4> Rut: </h4>
+                        <h4> Fono oficina: </h4>
+                        <h4> Titulo: </h4>
+                    </div>
+                </div>
+            </section>
+
+            {/* Footer provisorio */}
+            <footer>
+                <div> Mensaje </div>
+                <div> Contacto muni </div>
+            </footer>
         </div>
     );
 }
