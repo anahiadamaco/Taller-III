bd964a02ba53c75a84ec71e1ef4bd63a026c3669
anahiadamaco <anahiadamaco@gmail.com>
2024-10-13 19:38:13 -0300

    correción bd

--- backend/src/db.js
+++ backend/src/db.js
@@ -14,116 +14,90 @@ const pool = new Pool({
 const createTables = async () => {
   const sql = `
     CREATE TABLE IF NOT EXISTS "Rol" (
-      "rol" serial PRIMARY KEY,
-      "nombre_rol" varchar(50)
-    );
+  "rol" serial PRIMARY KEY,
+  "nombre_rol" varchar(20)
+);
 
-    CREATE TABLE IF NOT EXISTS "Usuario" (
-      "rut" varchar(11) PRIMARY KEY,
-      "nombre" varchar(20),
-      "apellidos" varchar(40),
-      "correo" varchar(64),
-      "telefono" int,
-      "contraseña" varchar(255),
-      "rol" int,
-      FOREIGN KEY ("rol") REFERENCES "Rol"("rol")
-    );
+CREATE TABLE IF NOT EXISTS "Usuario" (
+  "rut" varchar(11) PRIMARY KEY,
+  "nombre" varchar(20),
+  "apellido_paterno" varchar(40),
+  "apellido_materno" varchar(40),
+  "correo" varchar(64),
+  "telefono" int,
+  "contraseña" varchar(256),
+  "rol" serial,
+  FOREIGN KEY ("rol") REFERENCES "Rol"("rol")
+);
 
-    CREATE TABLE IF NOT EXISTS "Persona_Mayor" (
-      "reg_paciente" int PRIMARY KEY,
-      "rut" varchar(11),
-      "f_nac" date,
-      "direccion" varchar(40),
-      "reg_soc_hogar" int,
-      "mov_reducida" boolean,
-      "cont_atenciones" int,
-      FOREIGN KEY ("rut") REFERENCES "Usuario"("rut")
-    );
+CREATE TABLE IF NOT EXISTS "Cita" (
+  "id_cita" serial PRIMARY KEY,
+  "reg_paciente" int,
+  "id_hora" serial,
+  "estado" varchar(20),
+  "asistencia" boolean,
+  "comentario" text,
+  "cont_asistencia" boolean
+);
 
-    CREATE TABLE IF NOT EXISTS "Prestador_Servicio" (
-      "reg_prestrador" int PRIMARY KEY,
-      "rut" varchar(11),
-      "id_servicio" varchar(20),
-      FOREIGN KEY ("rut") REFERENCES "Usuario"("rut")
-    );
+CREATE TABLE IF NOT EXISTS "Notificacion" (
+  "id_noti" serial PRIMARY KEY,
+  "reg_prestador" int,
+  "reg_paciente" int,
+  "id_cita" int,
+  "tipo_noti" varchar(10),
+  "texto" text,
+  "f_envio" timestamp,
+  FOREIGN KEY ("id_cita") REFERENCES "Cita"("id_cita")
+);
 
-    CREATE TABLE IF NOT EXISTS "Horario" (
-      "id_hora" serial PRIMARY KEY,
-      "reg_prestador" int,
-      "fecha_hora" timestamp,
-      "disponible" boolean,
-      FOREIGN KEY ("reg_prestador") REFERENCES "Prestador_Servicio"("reg_prestrador")
-    );
+CREATE TABLE IF NOT EXISTS "Persona_Mayor" (
+  "reg_paciente" serial PRIMARY KEY,
+  "rut" Varchar(11),
+  "f_nac" date,
+  "direccion" varchar(40),
+  "reg_soc_hogar" int,
+  "mov_reducida" boolean,
+  "cont_atenciones" int,
+  FOREIGN KEY ("rut") REFERENCES "Usuario"("rut")
+);
 
-    CREATE TABLE IF NOT EXISTS "Cita" (
-      "id_cita" serial PRIMARY KEY,
-      "reg_paciente" int,
-      "id_hora" int,
-      "estado" varchar(20),
-      "asistencia" boolean,
-      "comentario" text,
-      "cont_asistencia" boolean,
-      FOREIGN KEY ("reg_paciente") REFERENCES "Persona_Mayor"("reg_paciente"),
-      FOREIGN KEY ("id_hora") REFERENCES "Horario"("id_hora")
-    );
+CREATE TABLE IF NOT EXISTS "Prestador_Servicio" (
+  "reg_prestador" int PRIMARY KEY,
+  "rut" varchar(11),
+  "id_servicio" varchar(20),
+  FOREIGN KEY ("rut") REFERENCES "Usuario"("rut")
+);
 
-    CREATE TABLE IF NOT EXISTS "Notificacion" (
-      "id_noti" serial PRIMARY KEY,
-      "reg_prestador" int,
-      "reg_paciente" int,
-      "id_cita" int,
-      "tipo_noti" varchar(10),
-      "texto" text,
-      "f_envio" timestamp,
-      FOREIGN KEY ("id_cita") REFERENCES "Cita"("id_cita"),
-      FOREIGN KEY ("reg_paciente") REFERENCES "Persona_Mayor"("reg_paciente"),
-      FOREIGN KEY ("reg_prestador") REFERENCES "Prestador_Servicio"("reg_prestrador")
-    );
+CREATE TABLE IF NOT EXISTS "Horario" (
+  "id_hora" serial PRIMARY KEY,
+  "reg_prestador" int,
+  "fecha_hora" timestamp,
+  "disponible" boolean,
+  FOREIGN KEY ("reg_prestador") REFERENCES "Prestador_Servicio"("reg_prestador")
+);
 
-    CREATE TABLE IF NOT EXISTS "Report_Estadisticas" (
-      "id_reporte" serial PRIMARY KEY,
-      "id_cita" int,
-      "cant_atenciones" int,
-      FOREIGN KEY ("id_cita") REFERENCES "Cita"("id_cita")
-    );
-
-    CREATE TABLE IF NOT EXISTS "Form_Mov_Reducida" (
-      "mov_reducida" boolean,
-      "tipo" varchar(20),
-      "acompañante" boolean,
-      PRIMARY KEY ("mov_reducida")
-    );
+CREATE TABLE IF NOT EXISTS "Form_Mov_Reducida" (
+  "id_form" serial PRIMARY KEY,
+  "reg_paciente" int,
+  "tipo" varchar(20),
+  "acompañante" boolean,
+  FOREIGN KEY ("reg_paciente") REFERENCES "Persona_Mayor"("reg_paciente")
+);
   `;
 
   try {
+    // Ejecutar la consulta
     await pool.query(sql);
-    console.log('Tablas creadas correctamente');
+    console.log("Tablas creadas o existentes.");
   } catch (err) {
-    console.error('Error creando tablas:', err);
+    console.error('Error creando las tablas:', err);
   }
 };
 
-async function insertarRoles() {
-  const query = `
-      INSERT INTO "Rol" (nombre_rol)
-      VALUES 
-          ('personamayor'),
-          ('prestador'),
-          ('administrador');
-  `;
-
-  try {
-      // Ejecutar la consulta
-      await pool.query(query);
-      console.log("Roles insertados exitosamente.");
-  } catch (err) {
-      console.error('Error insertando roles:', err);
-  }
-}
-
 const initializeDatabase = async () => {
   await createTables();
-  await insertarRoles();
+  // Aquí puedes agregar más funciones para insertar roles, datos de prueba, etc.
 };
 
 initializeDatabase();
