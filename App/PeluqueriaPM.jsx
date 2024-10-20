ec013ffb423577b50478e827a518d8d1e59fac52
IssPame19 <issabelpamela4e@gmail.com>
2024-10-20 14:15:46 -0300

    Pagina Kine y Peluquería listas
    Con uso de un compilador online

--- App/PeluqueriaPM.jsx
+++ App/PeluqueriaPM.jsx
@@ -1,42 +1,60 @@
-import React from 'react';
-import { View, Text, Button, ScrollView } from 'react-native';
+import React from "react";
+import { View, Text, Button, ScrollView } from "react-native";
+import tailwind from "tailwind-rn";
 
 const PeluqueriaPM = () => {
   return (
-    <ScrollView>
+    <ScrollView style={tailwind("bg-gray-100")}>
       {/* Nav */}
 
       {/* Header */}
-      <View>
-        <Text>Bienvenido a Peluqueria</Text>
-        <Text>Cuidado capilar para tu bienestar.</Text>
+      <View style={tailwind("bg-blue-500 p-6")}>
+        <Text style={tailwind("text-white text-4xl font-bold mb-2 text-center")}>
+          Bienvenido a Peluquería
+        </Text>
+
+        <Text style={tailwind("text-white text-2xl text-center")}>
+          Cuidado capilar para tu bienestar.
+        </Text>
       </View>
 
       {/* Descripcion */}
-      <View>
-        <Text>Peluqueria</Text>
-        <Text>Ofrecemos servicios profesionales de peluqueria para todas tus necesidades capilares.</Text>
+      <View style={tailwind("bg-white p-4 my-4 rounded-lg")}>
+        <Text style={tailwind("text-blue-700 text-2xl font-bold mb-2 text-center")}>
+          Peluquería
+        </Text>
+
+        <Text style={tailwind("text-xl")}>
+          Ofrecemos servicios profesionales de peluquería para todas tus
+          necesidades capilares.
+        </Text>
       </View>
 
       {/* Servicios */}
-      <View>
-        <Text>Servicios Disponibles</Text>
-        <Text>• Cortes de cabello</Text>
-        <Text>• Coloración</Text>
-        <Text>• Tratamientos capilares</Text>
+      <View style={tailwind("bg-white p-4 my-4 rounded-lg")}>
+        <Text style={tailwind("text-blue-700 text-2xl font-bold mb-2 text-center")}>
+          Servicios Disponibles
+        </Text>
+        <Text style={tailwind("text-xl")}>• Cortes de cabello</Text>
+        <Text style={tailwind("text-xl")}>• Coloración</Text>
+        <Text style={tailwind("text-xl")}>• Tratamientos capilares</Text>
       </View>
 
       {/* Horario */}
-      <View>
-        <Text>Seleccionar Horario</Text>
-        <Button title="Ver Calendario" onPress={() => {}} />
+      <View style={tailwind("bg-white p-4 my-4 rounded-lg")}>
+        <Text style={tailwind("text-blue-700 text-2xl font-bold mb-2 text-center")}>
+          Seleccionar Horario
+        </Text>
+        <Button style={tailwind("text-xl")} title="Ver Calendario" onPress={() => {}} />
       </View>
 
       {/* Especialistas */}
-      <View>
-        <Text>Especialistas</Text>
-        <Text>Ana Perez - Cortes de cabello</Text>
-        <Text>Luis Martínez - Tinturas y mechas</Text>
+      <View style={tailwind("bg-white p-4 my-4 rounded-lg")}>
+        <Text style={tailwind("text-blue-700 text-2xl font-bold mb-2 text-center")}>
+          Especialistas
+        </Text>
+        <Text style={tailwind("text-xl")}>Ana Pérez - Cortes de cabello</Text>
+        <Text style={tailwind("text-xl")}>Luis Martínez - Tinturas y mechas</Text>
       </View>
     </ScrollView>
   );
