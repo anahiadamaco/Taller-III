23704ae4728a46162e350dd016536960dc5fa916
IssPame19 <issabelpamela4e@gmail.com>
2024-10-20 23:57:41 -0300

    Arreglos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,17 +6,16 @@ import Login from './pages/Login';
 /* Rutas Administrador */
 import HomeAdmin from './pages/HomeAdmin';
 import GestionarPS from './pages/GestionarPS';
-import Servicios from './pages/Admin_servicios';
+import Servicios from './pages/Admin_Servicios';
 import GenerarReportes from "./pages/generador_reportes";
 
 /* Rutas Prestador de Servicios */
 import HomePS from './pages/HomePS';
-/* import RegiPS from './pages/RegistroPS'; */
 import AsJuPS from './pages/AsistenciaJuridicaPS';
-/* import AsSoPS from './pages/AsistenciaSocialPS'; */
-/* import FonoPS from './pages/FonoaudiologiaPS'; */
+import AsSoPS from './pages/AsistenciaSocialPS';
+import FonoPS from './pages/FonoaudiologiaPS';
 import KinePS from './pages/KinesiologiaPS';
-/* import PeluPS from './pages/PeluqueriaPS'; */
+import PeluPS from './pages/PeluqueriaPS';
 import PodoPS from './pages/PodologiaPS';
 import PsicPS from './pages/PsicologiaPS';
 
@@ -24,8 +23,7 @@ import PsicPS from './pages/PsicologiaPS';
 import HomePM from './pages/HomePM';
 import RegiPM from './pages/RegistroPM';
 import AsJuPM from './pages/AsistenciaJuridicaPM';
-import AsSoPM from './pages/AsistenciaSocialPM'
-/* import AsSoPM from './pages/AsistenciaSocialPM'; */
+import AsSoPM from './pages/AsistenciaSocialPM';
 import FonoPM from './pages/FonoaudiologiaPM';
 import KinePM from './pages/KinesiologiaPM';
 import PeluPM from './pages/PeluqueriaPM';
@@ -47,12 +45,11 @@ function App() {
                 
                 {/* Rutas Prestador de Servicios */}
                 <Route path="/HPS" element={<HomePS />} />
-                {/* <Route path="/RPS" element={<RegiPS />} /> */}
                 <Route path="/AJPS" element={<AsJuPS />} />
-                {/* <Route path="/ASPS" element={<AsSoPS />} /> */}
-                {/* <Route path="/FPS" element={<FonoPS />} /> */}
+                <Route path="/ASPS" element={<AsSoPS />} />
+                <Route path="/FPS" element={<FonoPS />} />
                 <Route path="/KPS" element={<KinePS />} />
-                {/* <Route path="/PEPS" element={<PeluPS />} /> */}
+                <Route path="/PEPS" element={<PeluPS />} />
                 <Route path="/POPS" element={<PodoPS />} />
                 <Route path="/PSPS" element={<PsicPS />} />
 
