b47162b31099a144f73be647d3c548b3397a5e0a
anahiadamaco <anahiadamaco@gmail.com>
2024-10-24 20:09:53 -0300

    .

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME, // nombre de la base de datos
-    process.env.DB_USER, // usuario correcto (debería ser "postgres")
-    process.env.DB_PASSWORD, // contraseña correcta
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, 
     {
         host: process.env.DB_HOST,
         dialect: 'postgres',
