c55a588c36b88ac295b6a6c8af3eacb72da62189
IssPame19 <issabelpamela4e@gmail.com>
2024-10-30 16:25:46 -0300

    Arreglos rutas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,48 +6,40 @@ import Login from './pages/Login';
 /* Rutas Administrador */
 import HomeAdmin from './pages/HomeAdmin';
 import GestionarPS from './pages/GestionarPS';
-import Servicios from './pages/Admin_Servicios';
+import Servicios from './pages/Admin_servicios';
 import GenerarReportes from "./pages/generador_reportes";
 
 /* Rutas Prestador de Servicios */
 import HomePS from './pages/HomePS';
-import AsJuPS from './pages/AsistenciaJuridicaPS';
+import AsJuPS from './pages/AsistenciaJuridicaps';
 import AsSoPS from './pages/AsistenciaSocialPS';
 import FonoPS from './pages/FonoaudiologiaPS';
 import KinePS from './pages/KinesiologiaPS';
-
 import PeluPS from './pages/PeluqueriaPS';
-
-import PodoPS from './pages/PodologiaPS';
+import PodoPS from './pages/Podologiaps';
 import PsicPS from './pages/PsicologiaPS';
 
 /* Rutas Persona Mayor */
 import HomePM from './pages/HomePM';
-import RegiPM from './pages/RegistroPM';
+import RegiPM from './pages/Registropm';
 import AsJuPM from './pages/AsistenciaJuridicaPM';
-
 import AsSoPM from './pages/AsistenicaSocialPM';
-
 import FonoPM from './pages/FonoaudiologiaPM';
 import KinePM from './pages/KinesiologiaPM';
-import PeluPM from './pages/PeluqueriaPM';
+import PeluPM from './pages/Peluqueriapm';
 import PodoPM from './pages/PodologiaPM';
-import PsicPM from './pages/PsicologiaPM';
+import PsicPM from './pages/Psicologiapm';
 import FormularioMovilidad from "./pages/Fomulario_Movi";
-
 
 /* Para probar funcionamiento */
 import Graf from './component/Grafico';
 import Guia from './component/GuiaPM';
 import PerPM from './pages/PerfilUsuarios';
-
 import RegisSoc from './pages/RegistroSocialHogar';
-
 import PerPS from './pages/PerfilPrestadorServicios';
 import FormAV from './component/FormAggVac';
 import FormEPPS from './component/FormEdPerfPS';
 import FormEPPM from './component/FormEdPerfPM';
-
 import Evaluacion_Servicios from "./component/Evaluacion_servicio"; 
 
 
@@ -55,7 +47,6 @@ function App() {
     return (
         <Router>
             <Routes>
-
                 <Route path="/EvaluacionServicios" element={<Evaluacion_Servicios/>}/>
 
                 <Route path="/" element={<Login />} />
@@ -80,7 +71,6 @@ function App() {
                 <Route path="/HPM" element={<HomePM />} />
                 <Route path="/RPM" element={<RegiPM />} />
                 <Route path="/AJPM" element={<AsJuPM />} />
-
                 <Route path="/ASPM" element={<AsSoPM />} /> 
                 <Route path="/FPM" element={<FonoPM />} />
                 <Route path="/KPM" element={<KinePM />} />
