fdb68a7423621d64eaa5472be528bfe9a61b8531
IssPame19 <issabelpamela4e@gmail.com>
2024-11-19 02:15:55 -0300

    Conexion BD, parte 1

--- Intregra3/app/build.gradle.kts
+++ Intregra3/app/build.gradle.kts
@@ -75,9 +75,6 @@ dependencies {
     implementation ("androidx.compose.material3:material3:1.1.1")
     implementation ("androidx.navigation:navigation-compose:2.6.0")
     implementation ("androidx.compose.material3:material3:1.1.0")
-
-
-
-
-
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 }
