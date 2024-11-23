5b3d57215a42256a7131cee00b033560a990697b
Demian Binimelis <dbinimelis2022@alu.uct.cl>
2024-11-22 21:09:01 -0300

    Configuracion de headers para apartados de prestadores de servicio

--- frontend/src/pages/HomePS.jsx
+++ frontend/src/pages/HomePS.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import Footer from '../component/FooterPS';
+import { Outlet, Link } from 'react-router-dom';
+import logo from '../img/logo_muni.webp';
 
 function Home() {
     return (
         <div className="flex flex-col min-h-screen">
-            {/* Header provisorio */}
-            <header className='flex justify-between bg-red-200 text-center gap-2 p-4'>
-                <div className='w-1/3'> Logo Temuco </div>
-                <div className='w-1/3'> Bienvenid@ (Nombre) </div>
-                <div className='w-1/3'> Cerrar sesion </div>
-            </header>
+            {/* Encabezado de la página */}
+            <section className='z-20'> 
+                <header className="flex items-center bg-black h-24 shadow-md px-8">
+                    <div className="flex items-center">
+                        <img src={logo} alt="Logo" className="h-12 w-auto" />
+                    </div>
+
+
+                    {/* Navegación principal */}
+                    <nav id="Menu1" className="ml-auto flex items-center space-x-8 text-xl">
+                        {/* Botón de Inicio */}
+                        <Link
+                            to="/HPS"
+                            className="px-4 py-1 text-center text-white duration-300 hover:text-sky-600 border-b-2 border-transparent hover:border-sky-600"
+                        >
+                            Inicio
+                        </Link>
+
+                        <Link
+                            to="/AJPS"
+                            className="px-4 py-1 text-center text-white duration-300 hover:text-sky-600 border-b-2 border-transparent hover:border-sky-600"
+                        >
+                            Zona de Administración
+                        </Link>
+
+                        {/* Botón de Cerrar Sesión */}
+                        <Link
+                            to="/"
+                            className="px-4 py-1 text-center text-white duration-300 hover:text-sky-600 border-b-2 border-transparent hover:border-sky-600"
+                        >
+                            Cerrar Sesión
+                        </Link>
+                    </nav>
+                </header>
+                <Outlet />
+            </section>
 
             <div className="min-h-screen grid grid-rows-[auto_1fr] bg-gray-100">
                 <header>
